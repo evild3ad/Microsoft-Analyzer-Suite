899adae714e20cf82e94f807487f6d759c7878c2
evild3ad <evild3ad79@gmail.com>
2024-04-20 08:58:52 +0200

    2024-04-20

---
+++ TransportRules-Analyzer.ps1
@@ -0,0 +1,288 @@
+﻿# TransportRules-Analyzer v0.1
+#
+# @author:    Martin Willing
+# @copyright: Copyright (c) 2024 Martin Willing. All rights reserved.
+# @contact:   Any feedback or suggestions are always welcome and much appreciated - mwilling@lethal-forensics.com
+# @url:       https://lethal-forensics.com/
+# @date:      2024-04-20
+#
+#
+# ██╗     ███████╗████████╗██╗  ██╗ █████╗ ██╗      ███████╗ ██████╗ ██████╗ ███████╗███╗   ██╗███████╗██╗ ██████╗███████╗
+# ██║     ██╔════╝╚══██╔══╝██║  ██║██╔══██╗██║      ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝██║██╔════╝██╔════╝
+# ██║     █████╗     ██║   ███████║███████║██║█████╗█████╗  ██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗██║██║     ███████╗
+# ██║     ██╔══╝     ██║   ██╔══██║██╔══██║██║╚════╝██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║██║██║     ╚════██║
+# ███████╗███████╗   ██║   ██║  ██║██║  ██║███████╗ ██║     ╚██████╔╝██║  ██║███████╗██║ ╚████║███████║██║╚██████╗███████║
+# ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝ ╚═════╝╚══════╝
+#
+#
+# Dependencies:
+#
+# ImportExcel v7.8.6 (2023-10-13)
+# https://github.com/dfinke/ImportExcel
+#
+# xsv v0.13.0 (2018-05-12)
+# https://github.com/BurntSushi/xsv
+#
+#
+# Changelog:
+# Version 0.1
+# Release Date: 2024-04-20
+# Initial Release
+#
+#
+# Tested on Windows 10 Pro (x64) Version 22H2 (10.0.19045.4291) and PowerShell 5.1 (5.1.19041.4291)
+#
+#
+#############################################################################################################################################################################################
+#############################################################################################################################################################################################
+
+<#
+.SYNOPSIS
+  TransportRules-Analyzer v0.1 - Automated Processing of M365 Transport Rules for DFIR
+
+.DESCRIPTION
+  TransportRules-Analyzer.ps1 is a PowerShell script utilized to simplify the analysis of Transport Rules (Exchange Online) extracted via "Microsoft Extractor Suite" by Invictus Incident Response.
+
+  https://github.com/invictus-ir/Microsoft-Extractor-Suite (Microsoft-Extractor-Suite v1.3.3)
+
+  https://microsoft-365-extractor-suite.readthedocs.io/en/latest/functionality/TransportRules.html
+
+.EXAMPLE
+  PS> .\TransportRules-Analyzer.ps1
+
+.NOTES
+  Author - Martin Willing
+
+.LINK
+  https://lethal-forensics.com/
+#>
+
+#############################################################################################################################################################################################
+#############################################################################################################################################################################################
+
+#region Declarations
+
+# Declarations
+
+# Script Root
+if ($PSVersionTable.PSVersion.Major -gt 2)
+{
+    # PowerShell 3+
+    $SCRIPT_DIR = $PSScriptRoot
+}
+else
+{
+    # PowerShell 2
+    $SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+}
+
+# Output Directory
+$OUTPUT_FOLDER = "$env:USERPROFILE\Desktop\TransportRules-Analyzer"
+
+# Tools
+
+# xsv
+$script:xsv = "$SCRIPT_DIR\Tools\xsv\xsv.exe"
+
+#endregion Declarations
+
+#############################################################################################################################################################################################
+
+#region Header
+
+# Windows Title
+$DefaultWindowsTitle = $Host.UI.RawUI.WindowTitle
+$Host.UI.RawUI.WindowTitle = "TransportRules-Analyzer v0.1 - Automated Processing of M365 Transport Rules for DFIR"
+
+# Check if the PowerShell script is being run with admin rights
+if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    Write-Host "[Error] This PowerShell script must be run with admin rights." -ForegroundColor Red
+    Exit
+}
+
+# Flush Output Directory
+if (Test-Path "$OUTPUT_FOLDER")
+{
+    Get-ChildItem -Path "$OUTPUT_FOLDER" -Force -Recurse -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse
+    New-Item "$OUTPUT_FOLDER" -ItemType Directory -Force | Out-Null
+}
+else 
+{
+    New-Item "$OUTPUT_FOLDER" -ItemType Directory -Force | Out-Null
+}
+
+# Function Get-FileSize
+Function Get-FileSize() {
+    Param ([long]$Length)
+    If ($Length -gt 1TB) {[string]::Format("{0:0.00} TB", $Length / 1TB)}
+    ElseIf ($Length -gt 1GB) {[string]::Format("{0:0.00} GB", $Length / 1GB)}
+    ElseIf ($Length -gt 1MB) {[string]::Format("{0:0.00} MB", $Length / 1MB)}
+    ElseIf ($Length -gt 1KB) {[string]::Format("{0:0.00} KB", $Length / 1KB)}
+    ElseIf ($Length -gt 0) {[string]::Format("{0:0.00} Bytes", $Length)}
+    Else {""}
+}
+
+# Select Log File
+Function Get-LogFile($InitialDirectory)
+{ 
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.InitialDirectory = $InitialDirectory
+    $OpenFileDialog.Filter = "Transport Rules|*-TransportRules.csv|All Files (*.*)|*.*"
+    $OpenFileDialog.ShowDialog()
+    $OpenFileDialog.Filename
+    $OpenFileDialog.ShowHelp = $true
+    $OpenFileDialog.Multiselect = $false
+}
+
+$Result = Get-LogFile
+
+if($Result -eq "OK")
+{
+    $script:LogFile = $Result[1]
+}
+else
+{
+    $Host.UI.RawUI.WindowTitle = "$DefaultWindowsTitle"
+    Exit
+}
+
+# Create a record of your PowerShell session to a text file
+Start-Transcript -Path "$OUTPUT_FOLDER\Transcript.txt"
+
+# Get Start Time
+$startTime = (Get-Date)
+
+# Logo
+$Logo = @"
+██╗     ███████╗████████╗██╗  ██╗ █████╗ ██╗      ███████╗ ██████╗ ██████╗ ███████╗███╗   ██╗███████╗██╗ ██████╗███████╗
+██║     ██╔════╝╚══██╔══╝██║  ██║██╔══██╗██║      ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝██║██╔════╝██╔════╝
+██║     █████╗     ██║   ███████║███████║██║█████╗█████╗  ██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗██║██║     ███████╗
+██║     ██╔══╝     ██║   ██╔══██║██╔══██║██║╚════╝██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║██║██║     ╚════██║
+███████╗███████╗   ██║   ██║  ██║██║  ██║███████╗ ██║     ╚██████╔╝██║  ██║███████╗██║ ╚████║███████║██║╚██████╗███████║
+╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝ ╚═════╝╚══════╝
+"@
+
+Write-Output ""
+Write-Output "$Logo"
+Write-Output ""
+
+# Header
+Write-Output "TransportRules-Analyzer v0.1 - Automated Processing of M365 Transport Rules for DFIR"
+Write-Output "(c) 2024 Martin Willing at Lethal-Forensics (https://lethal-forensics.com/)"
+Write-Output ""
+
+# Analysis date (ISO 8601)
+$script:AnalysisDate = [datetime]::Now.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss")
+Write-Output "Analysis date: $AnalysisDate UTC"
+Write-Output ""
+
+#endregion Header
+
+#############################################################################################################################################################################################
+
+#region Analysis
+
+# Transport Rules
+
+Function Start-Processing {
+
+# Input-Check
+if (!(Test-Path "$LogFile"))
+{
+    Write-Host "[Error] $LogFile does not exist." -ForegroundColor Red
+    Write-Host ""
+    Stop-Transcript
+    $Host.UI.RawUI.WindowTitle = "$DefaultWindowsTitle"
+    Exit
+}
+
+# Check File Extension
+$Extension = [IO.Path]::GetExtension($LogFile)
+if (!($Extension -eq ".csv" ))
+{
+    Write-Host "[Error] No CSV File provided." -ForegroundColor Red
+    Stop-Transcript
+    $Host.UI.RawUI.WindowTitle = "$DefaultWindowsTitle"
+    Exit
+}
+
+# Input Size
+$InputSize = Get-FileSize((Get-Item "$LogFile").Length)
+Write-Output "[Info]  Total Input Size: $InputSize"
+
+# Count rows of CSV (w/ thousands separators)
+[int]$Count = & $xsv count "$LogFile"
+$Rows = '{0:N0}' -f $Count
+Write-Output "[Info]  Total Lines: $Rows"
+
+# Processing Transport Rules
+Write-Output "[Info]  Processing Transport Rules ..."
+New-Item "$OUTPUT_FOLDER\TransportRules\XLSX" -ItemType Directory -Force | Out-Null
+
+# XLSX
+if (Get-Module -ListAvailable -Name ImportExcel)
+{
+    if (Test-Path "$LogFile")
+    {
+        if([int](& $xsv count -d "," "$LogFile") -gt 0)
+        {
+            $IMPORT = Import-Csv -Path "$LogFile" -Delimiter "," -Encoding UTF8
+            $IMPORT | Export-Excel -Path "$OUTPUT_FOLDER\TransportRules\XLSX\TransportRules.xlsx" -NoNumberConversion * -FreezeTopRow -BoldTopRow -AutoSize -AutoFilter -WorkSheetname "Transport Rules" -CellStyleSB {
+            param($WorkSheet)
+            # BackgroundColor and FontColor for specific cells of TopRow
+            $BackgroundColor = [System.Drawing.Color]::FromArgb(255,220,0)
+            Set-Format -Address $WorkSheet.Cells["A1:E1"] -BackgroundColor $BackgroundColor -FontColor Black
+            # HorizontalAlignment "Center" of columns A-E
+            $WorkSheet.Cells["A:E"].Style.HorizontalAlignment="Center"
+            }
+        }
+    }
+}
+
+# File Size (XLSX)
+if (Test-Path "$OUTPUT_FOLDER\TransportRules\XLSX\TransportRules.xlsx")
+{
+    $Size = Get-FileSize((Get-Item "$OUTPUT_FOLDER\TransportRules\XLSX\TransportRules.xlsx").Length)
+    Write-Output "[Info]  File Size (XLSX) : $Size"
+}
+
+# Stats
+[int]$TransportRules = (Import-Csv -Path "$LogFile" -Delimiter "," | Measure-Object).Count
+$Count = '{0:N0}' -f $TransportRules
+Write-Output "[Info]  $Count Transport Rules found"
+
+}
+
+Start-Processing
+
+#endregion Analysis
+
+#############################################################################################################################################################################################
+
+#region Footer
+
+# Get End Time
+$endTime = (Get-Date)
+
+# Echo Time elapsed
+Write-Output ""
+Write-Output "FINISHED!"
+
+$Time = ($endTime-$startTime)
+$ElapsedTime = ('Overall analysis duration: {0} h {1} min {2} sec' -f $Time.Hours, $Time.Minutes, $Time.Seconds)
+Write-Output "$ElapsedTime"
+
+# Stop logging
+Write-Host ""
+Stop-Transcript
+Start-Sleep 2
+
+# Reset Windows Title
+$Host.UI.RawUI.WindowTitle = "$DefaultWindowsTitle"
+
+#endregion Footer
+
+#############################################################################################################################################################################################
+#############################################################################################################################################################################################
